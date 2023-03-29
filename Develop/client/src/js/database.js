da8c6dee2b2ebd7f4e1dd223202ef3b1a9cd8ea7
Tom <thomasjhouf@gmail.com>
2023-03-28 21:39:47 -0400

    finished the README and fixed a route

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -33,6 +33,6 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 initdb();
